89f1d1f02dcef4f03202f1af7ededd95b8bd904c
Chaithu98 <113006319+Chaithu98@users.noreply.github.com>
2022-09-08 09:46:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,26 +29,6 @@ def insertDetails():
        return "Your name is : "+name +" and email is : "+ email +" and your account is successfully created"
     return render_template("Success.html")
 
-
-@app.route("/registration", methods=["GET"])
-def createUsers():
-    pass
-    # id = db.insert_one({
-    #     'name': request.json['name'],
-    #     'email': request.json['email']
-    # })
-    # print("=======================================")
-    # print(id.inserted_id)
-    # print("=======================================")
-
-    # #data = jsonify({'id':str(ObjectId(id.inserted_id)), 'msg':"User created successfully"})
-
-    # return Response(
-    #     mimetype="application/json",
-    #     status=201,
-    #     response=json.dumps({"message": "User created successfully", "id":str(id.inserted_id)})
-    # )
-
 if __name__ == "__main__":
     app.run(debug=True, port=2000)
 
