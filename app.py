bbf4cd637662178c3d12e766c69563cebe795d30
chaithu <chaithanya.a.com>
2022-09-16 10:08:19 +0530

    Takes order

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request, jsonify, Response, json
-from flask_pymongo import PyMongo, ObjectId
-import os
+from flask import Flask, render_template,flash, request,session
+from flask_pymongo import PyMongo
+from flask_session import Session
 
 app = Flask(__name__)
+app.secret_key='Hello cafe'
 
 app.config['MONGO_URI']="mongodb://localhost/Cafe"
 mongo = PyMongo(app)
 db = mongo.db.users
+db2=mongo.db.prices
+orders=mongo.db.orders
 
 @app.route("/",methods=['GET'])
 def index():
@@ -14,7 +17,6 @@ def index():
 
 @app.route("/users", methods=['GET'])
 def getUsers():
-
     return render_template("Display.html", newuser=db.find({},{"_id":0}))
 
 @app.route("/register", methods=['GET','POST'])
@@ -23,13 +25,52 @@ def insertDetails():
         name = request.form.get("name")
         email = request.form.get("email")
         password = request.form.get("password")
-        id = db.insert_one({
+        session['email']=email
+        mail=list(db.find({'email':email}))
+        if(mail):
+            flash(f'"email already exists please enter a new email"','danger')
+        db.insert_one({
         'name': name,
         'email': email,
         'password':password
         })
-        return "Your name is : "+str(name) +" and email is : "+ str(email) +" and your account is successfully created"
-    return render_template("Display.html")
+        return render_template("index.html")
+    return render_template("signup.html")
 
+@app.route("/Signin", methods=['GET','POST'])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        session['email']=email
+        if db.count_documents({'email':email})!=0 and db.count_documents({'password':password})!=0:
+            flash(f'Email id already exixts','danger')
+        return render_template("index.html")
+    return render_template("signin.html")
+
+def updateCost():
+    db2.insert_one({
+        'Item': 'Coffee',
+        'Cost':60,
+        })
+
+@app.route('/Total',methods=['GET','POST'])
+def totalItems():
+    if request.method == "POST":
+        ItemName = request.form.get("name")
+        quantity = int(request.form.get("quantity"))
+        cost=db2.find_one({'Item':ItemName},{'_id':0,'Cost':1})
+        cost=cost['Cost']
+        orders.insert_one({
+        'Item': ItemName,
+        'Cost':cost,
+        'Quantity':quantity,
+        })
+        return render_template("AddingOrders.html",display=db2.find({},{"_id":0}))
+    return render_template("Total.html")
+
+@app.route('/Adding',methods=['GET','POST'])
+def orderItems():
+    return render_template("AddingOrders.html",newuser=orders.find({},{"_id":0}),display=db2.find({},{"_id":0}))
 if __name__ == "__main__":
-    app.run(debug=True, port=2000)
+    app.run(debug=True, port=2000) 
